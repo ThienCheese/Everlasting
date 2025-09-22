8469be02102ffb08412443e2231478abc28a2b21
Quoocode <vanquoc05072005@gmail.com>
2025-09-22 02:45:34 +0000

    adjust structure

--- backend/src/routes/reports.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { revenueReport } from "../controllers/reportsController.js";
-
-const router = Router();
-
-router.get("/revenue", revenueReport);
-
-export default router;
